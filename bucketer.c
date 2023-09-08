9e4c0c2a12c5504262a991cff4fd6c00f9a7f687
Meenakshi-account <meenakshithenappan63@gmail.com>
2023-09-08 20:17:35 +0530

    First Commit

--- bucketer.c
+++ bucketer.c
@@ -24,32 +24,11 @@ struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBat
   return counts;
 }
 
-int main() {
-  // Example battery capacities
-  int capacities[] = {110, 100, 90, 70, 50};
-  int numBatteries = sizeof(capacities) / sizeof(capacities[0]);
 
-  struct CountsBySoH result = countBatteriesByHealth(capacities, numBatteries);
-
-  // Asserts to check the counts
-  assert(result.healthy == 3);
-  assert(result.exchange == 1);
-  assert(result.failed == 1);
-  // Add more asserts if needed
-
-  return 0;
-}
 In this code, the countBatteriesByHealth function takes an array of present capacities and the number of batteries as input. It iterates through the batteries, calculates their SoH as a percentage, and classifies them based on the given criteria. The counts are stored in the struct CountsBySoH structure, and this structure is returned from the function.
 
 In the main function, an example array of battery capacities is provided, and the countBatteriesByHealth function is called to count and classify the batteries. Asserts are used to check the counts, ensuring that the implementation is correct. You can add more test cases or asserts as needed.
 
-
-
-
-
-
-  return counts;
-}
 
 void testBucketingByHealth() {
   const int presentCapacities[] = {115, 118, 80, 95, 91, 77};
